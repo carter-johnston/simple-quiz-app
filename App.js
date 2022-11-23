9a8b71707def6022654db35daa5fbaa54cf86e10
Carter <johnston.j.carter@gmail.com>
2022-11-22 23:22:35 -0500

    added assets, scaffolded main page

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, ImageBackground, Image} from 'react-native';
+
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={[styles.container, {flexDirection: "column"}]}>
       <StatusBar style="auto" />
+      <View style={{flex: 1}}>
+          <Image source={require('./assets/corvette-logo.png')} style={styles.image}></Image>
+        </View>
+        <View style={{flex: 1}}>        
+          <Text>Merry Christmas Mom!</Text>
+          <Text>You think you know your Corvettes?</Text>
+        </View>
+        <View style={{flex: 1.3}}>
+          <Button
+            style={styles.button}
+            title="Take the Quiz"
+            onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
     </View>
   );
 }
@@ -13,8 +30,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    resizeMode: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: 'Black',
+  }
 });
